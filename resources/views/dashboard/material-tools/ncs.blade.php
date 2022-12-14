03c21e8613199465a2a39151864cfa82105590e3
Prasetya Hadi Saputra <phs@Prasetyas-MacBook-Pro.local>
2022-12-14 23:30:15 +0700

    add permission administraotor

--- resources/views/dashboard/material-tools/ncs.blade.php
+++ resources/views/dashboard/material-tools/ncs.blade.php
@@ -120,7 +120,8 @@ use Illuminate\Support\Facades\Auth;
                         <h4>Result Inc</h4>
                         <hr>
                         <div class="row">
-                            <button class="btn btn-primary" data-title="Add Inc" data-toggle="modal" data-target="#add-inc">
+                            <button class="btn btn-primary btnAdd" id="btn-add-inc" data-title="Add Inc" data-toggle="modal"
+                                data-target="#add-inc">
                                 Add Inc &nbsp; <i class="fas fa-plus"></i>
                             </button>
                         </div>
@@ -217,8 +218,8 @@ use Illuminate\Support\Facades\Auth;
                         <h4>Image</h4>
                         <hr>
                         <div class="row">
-                            <button type="button" class="float-left ml-2 btn btn-primary" data-toggle="modal"
-                                data-target="#add-image">
+                            <button type="button" id="btn-add-image" class="btnAdd float-left ml-2 btn btn-primary"
+                                data-toggle="modal" data-target="#add-image">
                                 Add Image &nbsp; <i class="fas fa-plus"></i>
                             </button>
                         </div>
@@ -305,7 +306,8 @@ use Illuminate\Support\Facades\Auth;
 
                         <hr>
                         <div class="row">
-                            <button class="btn btn-primary mr-2" data-toggle="modal" data-target="#add-char">
+                            <button id="btn-add-char" class="btnAdd btn btn-primary mr-2" data-toggle="modal"
+                                data-target="#add-char">
                                 Add Characteristic &nbsp; <i class="fas fa-plus"></i>
                             </button>
                         </div>
@@ -396,7 +398,8 @@ use Illuminate\Support\Facades\Auth;
                         <h4>Colloquial Name</h4>
                         <hr>
                         <div class="row">
-                            <button class="btn btn-primary" data-toggle="modal" data-target="#add-cn">
+                            <button id="btn-add-cn" class="btnAdd btn btn-primary" data-toggle="modal"
+                                data-target="#add-cn">
                                 Add CN &nbsp; <i class="fas fa-plus"></i>
                             </button>
                         </div>
@@ -739,6 +742,11 @@ use Illuminate\Support\Facades\Auth;
 
         <script>
             $(document).ready(function() {
+                if (groupName != `Administrator's`) {
+                    $(".btnAdd").css("display", "none")
+                } else {
+                    $(".btnAdd").css("display", "inline-block")
+                }
                 $("#main-menu-MNU25").addClass("nav-item menu-is-opening menu-open")
                 $("#subchild-MNU26").addClass("nav-link active")
             });
